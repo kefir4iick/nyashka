1a7397021b90281f3a016aadfaf47ff8bd02d9f4
kefir4iick <lizardm17@icloud.com>
2025-03-03 23:28:36 +0200

    change some names

--- prime.cs
+++ prime.cs
@@ -1,16 +1,17 @@
 using System;
 
-namespace HelloWorld
+namespace Program
 {
-    class Program
+    public class Prime
     {
-        static Random rand_value = new Random();
-        static void Main()
+        public static Random rand_value = new Random();
+
+        public static void Main()
         {
             Console.WriteLine("Enter number: ");
             int n = Convert.ToInt32(Console.ReadLine());
             int k = pick(n);
-            if(millerrabin(n,k))
+            if (millerrabin(n, k))
             {
                 Console.WriteLine("is prime");
             }
@@ -19,13 +20,13 @@ namespace HelloWorld
                 Console.WriteLine("isn't prime");
             }
         }
-        
-        static int pick(int n)
+
+        public static int pick(int n)
         {
             return (int)Math.Log(n) * 3;
         }
-        
-        static bool millerrabin(int n, int k)
+
+        public static bool millerrabin(int n, int k)
         {
             if (n <= 1 || (n % 2) == 0)
             {
@@ -39,26 +40,26 @@ namespace HelloWorld
             {
                 int t = n - 1;
                 int s = 0;
-                while(t % 2 == 0)
+                while (t % 2 == 0)
                 {
                     t = t / 2;
                     s = s + 1;
                 }
-                
+
                 for (int i = 0; i < k; i++)
                 {
-                    int x = rand_value.Next(2, n-2);
+                    int x = rand_value.Next(2, n - 2);
                     if (gcd(x, n) != 1)
                     {
                         return false;
                     }
-                    
+
                     int y = horner(x, t, n);
-                    if(y == 1 || y == n - 1)
+                    if (y == 1 || y == n - 1)
                     {
                         continue;
                     }
-                    bool val = true;   
+                    bool val = true;
                     for (int j = 0; j < s - 1; j++)
                     {
                         y = horner(y, 2, n);
@@ -73,16 +74,15 @@ namespace HelloWorld
                         }
                     }
                     if (val)
-                    { 
+                    {
                         return false;
                     }
                 }
                 return true;
-                
             }
         }
-        
-        static int horner(int a, int b, int c)
+
+        public static int horner(int a, int b, int c)
         {
             int result = 1;
             a = a % c;
@@ -100,10 +100,10 @@ namespace HelloWorld
 
             return result;
         }
-        
-        static int gcd(int a, int b)
+
+        public static int gcd(int a, int b)
         {
-            while(b != 0)
+            while (b != 0)
             {
                 int c = b;
                 b = a % b;
